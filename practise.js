0a821ae2b46072805de063f92317d0038a2ea6c7
luckkey <javvadiarya@gmail.com>
2025-05-06 10:53:09 +0530

    mypractice files

---
+++ practise.js
@@ -0,0 +1,32 @@
+let pandu = "i'm not a good boy "
+pandu = "yeah of course he is good boy"
+pandu = " may be"
+console.log(pandu)
+const leela = "actually he is good"
+
+console.log(leela)
+
+
+function calling(){
+    let lakshmi ="we can access"
+    lakshmi = "may be not"
+    const narayana = "im a student"
+    
+    console.log(lakshmi)
+    console.log(pandu)
+    console.log(narayana)
+    console.log(leela)
+
+}
+
+if (true) {
+    let blockVariable = "I am in block";
+    const alsoInBlock = "Me too";
+    var notActuallyBlocked = "I can escape!";
+    console.log(blockVariable);  }
+//console.log(blockVariable)
+//console.log(alsoInBlock)
+console.log(notActuallyBlocked)
+calling()
+// console.log(lakshmi)
+// console.log(narayana)
